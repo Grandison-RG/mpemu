489ebb1356c1119224f6ac8e60963d5cf63f12d9
Elvecent <kvalyavin@ya.ru>
2018-02-11 23:04:43 +0500

    Websockets server example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import qualified Network.WebSockets as WS
+import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "running..."
+    state <- newMVar newServerState
+    WS.runServer "127.0.0.1" 9160 $ application state
