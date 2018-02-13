b8e4f27f22491794b85b22ca8c27df5923050294
Elvecent <kvalyavin@ya.ru>
2018-02-13 23:46:12 +0500

    Receive bytestring instead of text; remove Text from imports and deps

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( application
     ) where
 
-import qualified Data.Text as T
+import Data.ByteString.Lazy
 import qualified Network.WebSockets as WS
 
 application :: WS.ServerApp
 application pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
-    msg <- WS.receiveData conn
-    putStrLn (T.unpack msg)
+    msg <- WS.receiveDataMessage conn
+    case msg of
+      WS.Text b t -> return ()
+      WS.Binary b -> print . unpack $ b
