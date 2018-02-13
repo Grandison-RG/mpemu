6f09b0f6c55f7032e5121bb3a78c73af3dde432e
Elvecent <kvalyavin@ya.ru>
2018-02-13 21:38:32 +0500

    Purge junk code and imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,9 @@
 module Main where
 
 import qualified Network.WebSockets as WS
-import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
 import Lib
 
 main :: IO ()
 main = do
     putStrLn "running..."
-    state <- newMVar newServerState
-    WS.runServer "127.0.0.1" 30036 $ application state
+    WS.runServer "127.0.0.1" 30036 application
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,50 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( application,
-      newServerState
+    ( application
     ) where
-      
-import Data.Text (Text)
-import Control.Exception (finally)
-import Control.Monad (forM_, forever)
-import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
+import qualified Data.Text as T
 import qualified Network.WebSockets as WS
 
-type Client = (Text, WS.Connection)
-type ServerState = [Client]
-
-newServerState :: ServerState
-newServerState = []
-
-numClients :: ServerState -> Int
-numClients = length
-
-clientExists :: Client -> ServerState -> Bool
-clientExists client = any ((== fst client) . fst)
-
-addClient :: Client -> ServerState -> ServerState
-addClient client clients = client : clients
-
-removeClient :: Client -> ServerState -> ServerState
-removeClient client = filter ((/= fst client) . fst)
-
-broadcast :: Text -> ServerState -> IO ()
-broadcast message clients = do
-    T.putStrLn message
-    forM_ clients $ \(_, conn) -> WS.sendTextData conn message
-
-application :: MVar ServerState -> WS.ServerApp
-application state pending = do
+application :: WS.ServerApp
+application pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
     msg <- WS.receiveData conn
-    clients <- readMVar state
     putStrLn (T.unpack msg)
-
-talk :: WS.Connection -> MVar ServerState -> Client -> IO ()
-talk conn state (user, _) = forever $ do
-    msg <- WS.receiveData conn
-    readMVar state >>= broadcast msg
