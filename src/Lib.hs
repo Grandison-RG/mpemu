063ececc226a6cf110f7750377e16b75024e5dd6
DanielRrr <chelsea2005joecole@icloud.com>
2018-02-13 14:51:01 +0300

    erased chat

--- src/Lib.hs
+++ src/Lib.hs
@@ -44,37 +44,7 @@ application state pending = do
     WS.forkPingThread conn 30
     msg <- WS.receiveData conn
     clients <- readMVar state
-    case msg of
-        _   | not (prefix `T.isPrefixOf` msg) ->
-                WS.sendTextData conn ("Wrong announcement" :: Text)
-
-            | any ($ fst client)
-                [T.null, T.any isPunctuation, T.any isSpace] ->
-                    WS.sendTextData conn ("Name cannot " `mappend`
-                        "contain punctuation or whitespace, and " `mappend`
-                        "cannot be empty" :: Text)
-
-            | clientExists client clients ->
-                WS.sendTextData conn ("User already exists" :: Text)
-                
-            | otherwise -> flip finally disconnect $ do
-
-               modifyMVar_ state $ \s -> do
-                   let s' = addClient client s
-                   WS.sendTextData conn $
-                       "Welcome! Users: " `mappend`
-                       T.intercalate ", " (map fst s)
-                   broadcast (fst client `mappend` " joined") s'
-                   return s'
-               talk conn state client
-          where
-            prefix     = "Hi! I am "
-            client     = (T.drop (T.length prefix) msg, conn)
-            disconnect = do
-                -- Remove client and return new state
-                s <- modifyMVar state $ \s ->
-                    let s' = removeClient client s in return (s', s')
-                broadcast (fst client `mappend` " disconnected") s
+    putStrLn (T.unpack msg)
 
 talk :: WS.Connection -> MVar ServerState -> Client -> IO ()
 talk conn state (user, _) = forever $ do
