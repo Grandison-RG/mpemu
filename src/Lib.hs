18c4e21bfc1a561a4fba64db24e1fed2930f9fc4
Elvecent <kvalyavin@ya.ru>
2018-02-16 12:50:42 +0500

    Implement continuous communication with client (send back received data)

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,12 +4,19 @@ module Lib
 
 import Data.ByteString.Lazy
 import qualified Network.WebSockets as WS
+import Control.Monad (forever)
+
+printAndSendBack :: WS.Connection -> ByteString -> IO ()
+printAndSendBack conn msg = do
+  print . unpack $ msg
+  WS.sendBinaryData conn msg
 
 application :: WS.ServerApp
 application pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
-    msg <- WS.receiveDataMessage conn
-    case msg of
-      WS.Text b t -> return ()
-      WS.Binary b -> print . unpack $ b
+    forever $ do
+      msg <- WS.receiveDataMessage conn
+      case msg of
+        WS.Text b _ -> printAndSendBack conn b
+        WS.Binary b -> printAndSendBack conn b
