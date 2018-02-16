4ec21fca8b5170550602d1a10096fff980df8eb8
Elvecent <kvalyavin@ya.ru>
2018-02-16 14:08:13 +0500

    Simplify code by using a different WS function

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,17 +6,11 @@ import Data.ByteString.Lazy
 import qualified Network.WebSockets as WS
 import Control.Monad (forever)
 
-printAndSendBack :: WS.Connection -> ByteString -> IO ()
-printAndSendBack conn msg = do
-  print . unpack $ msg
-  WS.sendBinaryData conn msg
-
 application :: WS.ServerApp
 application pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
     forever $ do
-      msg <- WS.receiveDataMessage conn
-      case msg of
-        WS.Text b _ -> printAndSendBack conn b
-        WS.Binary b -> printAndSendBack conn b
+      msg <- WS.receiveData conn
+      print . unpack $ msg
+      WS.sendBinaryData conn msg
