573dcdee49e05837253c8c8ef4410ad77cc911fd
Elvecent <kvalyavin@ya.ru>
2018-02-18 00:40:09 +0500

    Add test response for test command: [0x0, 0xb9] -> [0x01, 0xb9, 0b101]

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BinaryLiterals #-}
 module Lib
     ( application
     ) where
@@ -7,8 +8,12 @@ import qualified Network.WebSockets as WS
 import Control.Monad (forever)
 
 dispatchRequest :: ByteString -> ByteString
-dispatchRequest msg = noSuchCommand
-  where noSuchCommand = pack [0, 255]
+dispatchRequest msg
+  | msg == testCommand = testResponse
+  | otherwise = noSuchCommand
+  where noSuchCommand = pack [0x0, 0xff]
+        testCommand = pack [0x00, 0xb9]
+        testResponse = pack [0x01, 0xb9, 0b101]
 
 application :: WS.ServerApp
 application pending = do
