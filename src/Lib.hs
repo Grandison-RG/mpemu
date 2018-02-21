db1231f55b981a10eed1044dc8abf9d2dbac4834
Elvecent <kvalyavin@ya.ru>
2018-02-21 19:05:30 +0500

    Add version cmd; print request AND response; introduce aux type synonims

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,17 +3,32 @@ module Lib
     ( application
     ) where
 
-import Data.ByteString.Lazy
+import Prelude hiding (length, tail)
+
+import Data.ByteString.Lazy hiding (putStrLn)
 import qualified Network.WebSockets as WS
+import qualified Data.ByteString.Lazy.Char8 as C
 import Control.Monad (forever)
 
-dispatchRequest :: ByteString -> ByteString
+type Command = ByteString
+type Response = ByteString
+
+dispatchRequest :: Command -> Response
 dispatchRequest msg
-  | msg == testCommand = testResponse
-  | otherwise = noSuchCommand
-  where noSuchCommand = pack [0x0, 0xff]
-        testCommand = pack [0x00, 0xb9]
-        testResponse = pack [0x01, 0xb9, 0b101]
+  | msg == testCmd = testRsp
+  | msg == versionCmd = versionRsp
+  | otherwise = noSuchCmd
+  where noSuchCmd = pack [0x00, 0xff]      :: Response
+
+        testRsp = pack [0x01, 0xb9, 0b101] :: Response
+        versionRsp = cons len datum        :: Response
+                     where
+                       datum = pack [0xa2, 0x08] `append`
+                               (C.pack "v1.0_emul_remote")
+                       len = fromIntegral . length $ datum
+
+        testCmd = pack [0x00, 0xb9]        :: Command
+        versionCmd = pack [0x00, 0xa2]     :: Command
 
 application :: WS.ServerApp
 application pending = do
@@ -21,6 +36,9 @@ application pending = do
     WS.forkPingThread conn 30
     forever $ do
       msg <- WS.receiveData conn
+      putStrLn "Request:"
       print . unpack $ msg
       let response = dispatchRequest msg
       WS.sendBinaryData conn response
+      putStrLn "Response:"
+      print . unpack $ response
