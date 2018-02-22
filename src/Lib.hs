c64c7d4d41d15ad71463153225cb792079c0bda8
Alexander Fedorov <universitas.literarum@gmail.com>
2018-02-22 19:22:22 +0500

    command number to its name

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,13 +5,17 @@ module Lib
 
 import Prelude hiding (length, tail)
 
-import Data.ByteString.Lazy hiding (putStrLn)
-import qualified Network.WebSockets as WS
+import           Data.ByteString.Lazy       hiding (putStrLn)
+import qualified Network.WebSockets         as WS
 import qualified Data.ByteString.Lazy.Char8 as C
-import Control.Monad (forever)
+import           Control.Monad (forever)
+import           Data.Word(Word8)
 
 type Command = ByteString
 type Response = ByteString
+
+data Cmd = VERSION | MOOLTIPASS_STATUS | ERR
+  deriving Show
 
 dispatchRequest :: Command -> Response
 dispatchRequest msg
@@ -30,17 +34,34 @@ dispatchRequest msg
         testCmd = pack [0x00, 0xb9]        :: Command
         versionCmd = pack [0x00, 0xa2]     :: Command
 
-emulVer :: String
-emulVer = "v1.0_emul_remote"
+-- emulVer :: String
+-- emulVer = "v1.0_emul_remote"
 
-dispatchRequest1 :: ByteString -> ByteString
-dispatchRequest1 msg = case ((\(_:x:_) -> x) . unpack $ msg) of
-                         0xb9 -> pack [0x01, 0xb9, 0b101]
-                         0xa2 -> let datum = pack [0xa2,0x08] `append` C.pack emulVer
-                                in
-                                  let len = fromIntegral (length  datum - 1)
-                                  in cons len datum
-                         _    -> pack [0x0, 0xff]
+moolipassStatus :: ByteString
+moolipassStatus = pack [0x01, 0xb9, 0b101]
+
+emulVer :: ByteString
+emulVer = cons len datum
+  where datum = pack [0xa2,0x08] `append` C.pack "v1.0_emul_remote"
+        len   = fromIntegral (length datum - 1)
+
+err :: ByteString
+err = pack [0x0, 0xff]
+
+secondByte :: ByteString -> Word8
+secondByte = (\(_:x:_) -> x) . unpack
+
+commandMap   :: ByteString -> Cmd
+commandMap c = case secondByte c of
+                0xA2 -> VERSION
+                0xB9 -> MOOLTIPASS_STATUS
+                _    -> ERR
+
+dispatchRequest1     :: ByteString -> ByteString
+dispatchRequest1 msg = case commandMap msg of
+                         MOOLTIPASS_STATUS -> moolipassStatus
+                         VERSION           -> emulVer
+                         ERR               -> err
 
 
                        
