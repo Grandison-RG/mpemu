c5bec1e6934fb6d38e5926e778584f3f5f11f9aa
Alexander Fedorov <universitas.literarum@gmail.com>
2018-02-23 17:24:27 +0500

    add '/0' to output str and add new dipatchRequest

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,30 +3,17 @@ module Lib
     ( application
     ) where
 
-import Prelude hiding (length, tail)
+import           Prelude                    hiding (length, tail)
 
 import           Data.ByteString.Lazy       hiding (putStrLn)
 import qualified Network.WebSockets         as WS
 import qualified Data.ByteString.Lazy.Char8 as C
 import           Control.Monad (forever)
 import           Data.Word(Word8)
+import           Data.Function(on)
 
 data Cmd = VERSION | MOOLTIPASS_STATUS | ERR
   deriving Show
-
--- emulVer :: String
--- emulVer = "v1.0_emul_remote"
-
-moolipassStatus :: ByteString
-moolipassStatus = pack [0x01, 0xb9, 0b101]
-
-emulVer :: ByteString
-emulVer = cons len datum
-  where datum = pack [0xa2,0x08] `append` C.pack "v1.0_emul_remote"
-        len   = fromIntegral (length datum - 1)
-
-err :: ByteString
-err = pack [0x0, 0xff]
 
 secondByte :: ByteString -> Word8
 secondByte = (\(_:x:_) -> x) . unpack
@@ -37,11 +24,17 @@ commandMap c = case secondByte c of
                 0xB9 -> MOOLTIPASS_STATUS
                 _    -> ERR
 
-dispatchRequest     :: ByteString -> ByteString
-dispatchRequest msg = case commandMap msg of
-                        MOOLTIPASS_STATUS -> moolipassStatus
-                        VERSION           -> emulVer
-                        ERR               -> err
+dispatchRequest   :: Cmd -> ByteString
+dispatchRequest c = case c of
+                      MOOLTIPASS_STATUS -> mooltipassStatus
+                      VERSION           -> emulVer
+                      ERR               -> err
+  where addNullChar str  = on append C.pack str "\0"  
+        mooltipassStatus = pack [0x01, 0xb9, 0b101]
+        emulVer          = cons len datum
+          where datum    = pack [0xa2,0x08] `append` addNullChar "v1.0_emul_remote"
+                len      = fromIntegral (length datum - 1)
+        err              = pack [0x0, 0xff]
 
 
 application :: WS.ServerApp
@@ -52,7 +45,7 @@ application pending = do
       msg <- WS.receiveData conn
       putStrLn "Request:"
       print . unpack $ msg
-      let response = dispatchRequest msg
+      let response = dispatchRequest . commandMap $ msg
       WS.sendBinaryData conn response
       putStrLn "Response:"
       print . unpack $ response
